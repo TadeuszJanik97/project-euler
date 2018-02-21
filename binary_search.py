a9322c2edd6695ad36b3f8aedea87a4e0a4bed25
Tadeusz Janik <tadeusz.janik.97@gmail.com>
2018-02-21 19:09:53 +0000

    Now it's actual binary search.

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,11 @@
+#only to be used with a list sorted in ascending order
 def search(l, element):
     if len(l) == 1:
         return l[0] == element
     n = int(len(l)/2)
-    return search(l[:n], element) or search(l[n:], element)
+    if l[n] == element:
+        return True
+    elif l[n] > element:
+        return search(l[:n], element)
+    else:
+        return search(l[n+1:], element)
